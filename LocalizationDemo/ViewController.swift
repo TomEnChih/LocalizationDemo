c9680183043cfd302f2f821e8e8d5139cf082b2b
tomtung <tomtung@nefer.com.tw>
2021-09-30 14:21:41 +0800

    建立 Demo

--- LocalizationDemo/ViewController.swift
+++ LocalizationDemo/ViewController.swift
@@ -11,7 +11,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        let label = UILabel()
+        label.frame = CGRect(x: self.view.frame.size.width / 2 - 100, y: self.view.frame.size.height / 2 - 25, width: 200, height: 50)
+        label.text = NSLocalizedString("TestLocalization", comment: "")
+        label.font = UIFont.systemFont(ofSize: 30)
+        label.textAlignment = NSTextAlignment.center
+
+        self.view.addSubview(label)
+        
+        let imageName = NSLocalizedString("CAT.jpeg", comment: "")
+        let image = UIImage(named: imageName)
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFit
+        imageView.frame = CGRect(x: self.view.frame.size.width / 2 - 100, y: label.frame.maxY + 20, width: 200, height: 200)
+        
+        view.addSubview(imageView)
+        
     }
 
 
